55b4acc6a5f6a31f058ad852afda4f3651d923dd
V Kapoor <vkapoor@spartaglobal.com>
2019-05-03 13:57:20 +0100

    Added selenium config and properties reader

---
+++ src/main/java/com/sparta/vk/seleniumConfig/seleniumConfig.java
@@ -0,0 +1,30 @@
+package com.sparta.vk.seleniumConfig;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class seleniumConfig {
+
+
+    public class SeleniumConfig
+    {
+        private WebDriver driver;
+        private SeleniumPropertiesReader propertiesReader;
+
+        public SeleniumConfig(String browser){
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+
+            driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        }
+
+        public WebDriver getDriver()
+        {
+            return driver;
+        }
+    }
+}
--- src/test/java/com/sparta/vk/stepdefs/CucumberStepdefs.java
+++ src/test/java/com/sparta/vk/stepdefs/CucumberStepdefs.java
@@ -12,6 +12,7 @@ public class CucumberStepdefs {
 
     private static BbcWebsite bbcWebsite;
 
+    @Test
    @Given("I am on the registration page")
     public void given() throws InterruptedException {
        System.setProperty("webdriver.chrome.driver", "C:\\Users\\VKapoor\\Documents\\chromedriver_win32\\chromedriver.exe");
@@ -23,14 +24,21 @@ public class CucumberStepdefs {
        bbcWebsite.registrationPage().clickContinueButton();
    }
 
+   @Test
    @When("I input the necessary details with the password details {string}")
     public void when(String password){
        bbcWebsite.registrationPage().inputPassword(password);
    }
 
+   @Test
    @Then("I receive the corresponding error {string}")
     public void then(String error){
        String errorMessage = bbcWebsite.registrationPage().getErrorMessage();
        Assert.assertEquals(errorMessage, error);
+   }
+
+   @AfterClass
+    public static void quitDriver() throws InterruptedException {
+       bbcWebsite.quitDriver();
    }
 }
